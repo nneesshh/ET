8fc623df8ba9ed2446df162f7f8ee1bf27930aa9
nnee <nneesshh@163.com>
2025-02-05 17:58:46 +0800

    luban

---
+++ tools/MiniTemplate/gen.bat
@@ -0,0 +1,16 @@
+set WORKSPACE=..
+set LUBAN_DLL=%WORKSPACE%\Tools\Luban\Luban.dll
+set CONF_ROOT=.
+
+dotnet %LUBAN_DLL% ^
+    -t all ^
+    -c cs-simple-json ^
+    -d json ^
+    --conf %CONF_ROOT%\luban.conf ^
+    -x outputCodeDir=..\..\Assets\Scripts\Luban ^
+    -x outputDataDir=..\..\Assets\StreamingAssets\Luban
+
+@REM 复制生成的文件到指定目录
+@REM xcopy .\out_server_jsons\* ..\..\immortal_publish\bin\res\jsons\ /E /Y
+
+pause
